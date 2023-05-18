f15fc752c2a20736e8da5e64daa16fdadb946777
tobiasfeldmann <132939959+tobiasfeldmann@users.noreply.github.com>
2023-05-18 12:58:43 +0200

    Anpassung der Reihenfolge von Antworten.

--- MarvelFragen.java
+++ MarvelFragen.java
@@ -3,17 +3,17 @@ public class MarvelFragen {
     static String[] antwortenMarvelFragen = {
         "Orange","Blau","Grün","Rot",
         "Erde", "Titan", "Nowhere", "Vormir",
-        "Surtur", "Hela", "Thor", "Thanos",
+        "Thanos", "Hela", "Thor", "Surtur",
         "Raumstein", "Gedankenstein", "Seelenstein", "Zeitstein",
         "Mercedes","Audi","Koenigsegg", "Lamborghini",
         "Brüssel","Berlin","Friedeburg","Budapest",
         "Mary-Jane", "Antonia","Cassie", "Jessica",
-        "14.000.605","293.421","420","11.088.205",
+        "14.000.605","293.421","16.180.420","11.088.205",
         "Monaco", "Madrid", "Lyon","Belgrad",
-        "untötbar","unverzeihlich","unvermeidbar","unabdingbar",
+        "untötbar","unverzeihlich","unvermeidbar","unverbesserlich",
         "Im Auge von Agamotto","Im Zahn von Ireldor","Im Tresor von Wakanda","In der Knieprothese der Ältesten",
-        "zu süß","bisschen trocken","zu nussig","ekelhaft",
-        "Palladium-Vergiftung","Diarrhoe","Rheuma","Folgen eines Herzinfarkts",
+        "zu süß","ekelhaft","zu nussig","bisschen trocken",
+        "Folgen eines Herzinfarkts","Diarrhoe","Rheuma","Palladium-Vergiftung",
         "5","6","7","8",
         "Eries Dactus Invictus Tangen Harus","Even Dead I'm The Hero","Eden Does Inherit The Home", "Eine Dattel ist tendenziell hochwachsend"
 
@@ -54,7 +54,7 @@ public class MarvelFragen {
         "An welcher Krankheit leidet Tony Stark zu Beginn von Iron Man 2?",
         "Wieviele Infinity Steine gibt es im MCU?",
         "Wofür steht E.D.I.T.H.?"
-        
+
     };
     
 }
